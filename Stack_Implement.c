6b8189f6e0ab8dfc77b352cc4d177552e4d21128
ParasAnxi <paras.bhandari1010@gmail.com>
2023-04-03 15:21:09 +0530

    stack

---
+++ Stack_Implement.c
@@ -0,0 +1,112 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define size 10
+int top = -1;
+int arr[size];
+
+void push(){
+    if(top == size-1){
+        printf("Overflow\n");
+    }
+    else{
+        int x;
+        printf("Enter the element : \n");
+        scanf("%d",&x);
+        top++;
+        arr[top] = x;
+    }
+}
+
+void pop(){
+    if(top == -1){
+        printf("Underflow\n");
+    }
+    else{
+        top--;
+    }
+}
+
+int peek(){
+    if(top == -1){
+        printf("Empty stack\n");
+    }
+    else{
+        printf("%d",arr[top]);
+    }
+}
+
+int isEmpty(){
+    if(top == -1){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+int isFull(){
+    if(top == size-1){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+void display(){
+    if(top == -1){
+        printf("Stack is empty\n");
+    }
+    else{
+        for(int i = top;i>=0;i--){
+            printf("%d ",arr[i]);
+        }printf("\n");
+    }
+}
+int main(){
+    int choice;
+    while(1){
+        printf("1 for push\n2 for pop\n3 for peek\n4 for empty\n5 for full\n6 for display\n7 for exit\n");
+        printf("Enter operation to perform : ");
+        printf("\n");
+        scanf("%d",&choice);
+
+        switch(choice){
+        case 1:
+            push();
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            peek();
+            break;
+        case 4:
+            if(isEmpty()){
+                printf("Stack is empty\n");
+            }
+            else{
+                printf("stack is not empty\n");
+            }
+            break;
+        case 5:
+            if(isFull()){
+                printf("Stack is full\n");
+            }
+            else{
+                printf("stack is not full\n");
+            }
+            break;
+        case 6:
+            display();
+            break;
+        case 7:
+            exit(0);
+            break;
+
+        default:
+        printf("Invalid choice\n");
+            break;
+        }
+    }
+}
