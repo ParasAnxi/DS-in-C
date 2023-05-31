46c9d4a0d2f3c4b7a03a048558364255a777c037
ParasAnxi <paras.bhandari1010@gmail.com>
2023-05-31 18:00:49 +0530

    BT

--- Tree/Level.c
+++ Tree/Level.c
@@ -39,28 +39,6 @@ void LevelOrder(struct Node* root){
 		temp_node = deQueue(queue, &front);
 	}
 }
-
-void inorderTraversal(struct Node* root) {
-  if (root == NULL) return;
-  inorderTraversal(root->left);
-  printf("%d ", root->data);
-  inorderTraversal(root->right);
-}
-
-void preorderTraversal(struct Node* root) {
-  if (root == NULL) return;
-  printf("%d ", root->data);
-  preorderTraversal(root->left);
-  preorderTraversal(root->right);
-}
-
-void postorderTraversal(struct Node* root) {
-  if (root == NULL) return;
-  postorderTraversal(root->left);
-  postorderTraversal(root->right);
-  printf("%d ", root->data);
-}
-
 struct Node* cons(int data) {
   struct Node* node = malloc(sizeof(struct Node));
   node->data = data;
@@ -82,24 +60,13 @@ struct Node* insertRight(struct Node* root, int data) {
 
 int main(){
   struct Node* root = cons(1);
-  insertLeft(root, 4);
-  insertRight(root, 6);
-  insertLeft(root->left, 42);
-  insertRight(root->left, 3);
-  insertLeft(root->right, 2);
-  insertRight(root->right, 33);
+  insertLeft(root, 2);
+  insertRight(root, 3);
+  insertLeft(root->left, 4);
+  insertRight(root->left, 5);
+  insertLeft(root->right, 6);
+  insertRight(root->right, 7);
 
   printf("LevelOrder traversal \n");
   LevelOrder(root);
-
-  printf("\nInorder traversal \n");
-  inorderTraversal(root);
-
-  printf("\nPreorder traversal \n");
-  preorderTraversal(root);
-
-
-  printf("\nPostorder traversal \n");
-  postorderTraversal(root);
-
 }
